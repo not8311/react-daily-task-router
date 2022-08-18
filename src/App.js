f60f07f4dcaa6d086e8176955002912db6cb6d88
阿君啊 <97567020+not8311@users.noreply.github.com>
2022-08-18 11:31:35 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import {
   HashRouter,
   NavLink,
+  Routes,
+  Route,
+  useNavigate
 } from 'react-router-dom';
 
 const Todo = () => {
-  return <p>這是 Todo 頁面 
-  </p>;
+  return (
+    <>
+      <p>這是 Todo 頁面 </p>
+      <Layout />
+    </>
+  );
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
@@ -14,6 +21,12 @@ const Login = () => {
 const Register = () => {
   return <p>這是註冊頁面</p>;
 };
+const Layout = () =>{
+  let navigate = useNavigate();
+  <button onClick={()=>navigate("/login")}>
+    登出
+  </button>
+}
 
 function App() {
   return (
